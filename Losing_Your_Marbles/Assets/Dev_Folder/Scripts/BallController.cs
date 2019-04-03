2a6b65ef201934889f0af2c3a1784e2ed0d65fb5
Cameron Simmons <cbsimmons4@gmail.com>
2019-04-03 00:07:03 -0400

    Initial Commit

---
+++ Losing_Your_Marbles/Assets/Dev_Folder/Scripts/BallController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BallController : MonoBehaviour
+{
+    List<KeyValuePair<int, int>> open_spaces;
+    Vector3 destination;
+    int[,] map;
+    NavMeshAgent agent;
+    MapGenerator mg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        mg = GameObject.Find("Map Generator").GetComponent<MapGenerator>();
+        open_spaces = mg.Get_OS();
+        map = mg.Get_map();
+        agent.enabled = true;
+        this.PickNewDistination();
+    }
+    void PickNewDistination()
+    {
+        KeyValuePair<int, int> space = open_spaces[Random.Range(0, open_spaces.Count)];
+        this.destination = new Vector3(space.Key - mg.width/2, 0, space.Value - mg.height/2);
+        agent.SetDestination(this.destination);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0,100) < 0.001f || Input.GetKey(KeyCode.P)|| 
+            agent.path.status == NavMeshPathStatus.PathInvalid || 
+            Vector3.Distance(agent.transform.position, destination) < 1) {
+            this.PickNewDistination();
+        }
+    }
+
+}
