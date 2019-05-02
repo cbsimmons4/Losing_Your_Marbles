f5c3ddb5aefc3247c7112b3913f854856b369011
Cameron Simmons <cbsimmons4@gmail.com>
2019-05-02 02:28:21 -0400

    Main Menu Almost Done

--- Losing_Your_Marbles/Assets/Dev_Folder/Scripts/GameManager.cs
+++ Losing_Your_Marbles/Assets/Dev_Folder/Scripts/GameManager.cs
@@ -9,48 +9,63 @@ public class GameManager : MonoBehaviour
 {
 
     [Range(0, 75)]
-    public static int enemy_cap;
+    public int enemy_cap;
 
     [Range(1, 50)]
-    public static int num_marbles;
+    public int num_marbles;
 
     [Range(100, 255)]
-    public static int width;
+    public int width;
 
     [Range(100, 255)]
-    public static int height;
+    public int height;
 
     [Range(5, 10)]
-    public static int passageWidth;
+    public int passageWidth;
 
-    public static string seed;
+    public string seed;
 
-    public static bool useRandomSeed;
+    public bool useRandomSeed;
 
     [Range(35, 55)]
-    public static int randomFillPercent;
+    public int randomFillPercent;
 
     [Range(20, 100)]
-    public static int wallThresholdSize;
+    public int wallThresholdSize;
 
     [Range(20, 100)]
-    public static int roomThresholdSize;
+    public int roomThresholdSize;
 
     [Range(0, 3)]
-    public static int forest_density;
+    public int forest_density;
 
-    public static bool disableMinimap;
+    public bool disableMinimap;
 
-    public void SetDefault() {
+    public void SetDefaultSettings() {
+        this.width = 155;
         widthSlider.value = 155;
+        this.height = 155;
         heightSlider.value = 155;
+        this.num_marbles = 15;
         marbleCountSlider.value = 15;
+        this.enemy_cap = 25;
         enemyCapSlider.value = 25;
+        this.passageWidth = 6;
         passageWidthSlider.value = 6;
+        this.randomFillPercent = 48;
         randomFillSlider.value = 48;
+        this.wallThresholdSize = 50;
         wallThreshholdSlider.value = 50;
+        this.roomThresholdSize = 50;
         roomThreshholdSlider.value = 50;
+        this.forest_density = 2;
         forestDensitySlider.value = 2;
+        this.useRandomSeed = true;
+        randomSeedToggle.isOn = true;
+        this.seed = "Enter Seed...";
+        seedInput.text = "Enter Seed...";
+        this.disableMinimap = false;
+        //miniMapToggle.isOn = false;
     }
 
     public Slider widthSlider;
@@ -64,16 +79,39 @@ public class GameManager : MonoBehaviour
     public Slider passageWidthSlider;
     public Slider wallThreshholdSlider;
     public Slider roomThreshholdSlider;
+
     public Toggle randomSeedToggle;
-   
+    public InputField seedInput;
+
+   // public Toggle miniMapToggle;
+
+    public Button resetButton;
+
+    public Button toSettingsButton;
+
+    public Button backButton;
+
+    public Button playButton;
+
+    public Button quitButton;
+
+   //public Slider LoadingSlider;
+
+    private GameObject MainMenu;
+    private GameObject SettingsMenu;
+    private GameObject LoadingMenu;
+
+
+
 
     private void Start()
     {
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(0))
-        {
-            SetDefault();
+            this.MainMenu = GameObject.Find("MainMenu");
+            this.SettingsMenu = GameObject.Find("SettingsMenu");
+            this.LoadingMenu = GameObject.Find("Loading");
 
-            randomSeedToggle.onValueChanged.AddListener(delegate { SetUseRandomSeed(randomSeedToggle.isOn); });
+        SetDefaultSettings();
+
             widthSlider.onValueChanged.AddListener(delegate { SetWidth((int)widthSlider.value); });
             heightSlider.onValueChanged.AddListener(delegate { SetHeigth((int)heightSlider.value); });
             marbleCountSlider.onValueChanged.AddListener(delegate { SetNumMarbles((int)marbleCountSlider.value); });
@@ -81,9 +119,74 @@ public class GameManager : MonoBehaviour
 
             randomFillSlider.onValueChanged.AddListener(delegate { SetRandomFillPercent((int)randomFillSlider.value); });
             forestDensitySlider.onValueChanged.AddListener(delegate { SetForestDensity((int)forestDensitySlider.value); });
-        }
+
+            randomSeedToggle.onValueChanged.AddListener(delegate { SetUseRandomSeed(randomSeedToggle.isOn); });
+            seedInput.onValueChanged.AddListener(delegate { SetSeed(seedInput.text); });
+            //miniMapToggle.onValueChanged.AddListener(delegate { SetMapStatus(miniMapToggle.isOn); });
+
+            resetButton.onClick.AddListener(delegate { SetDefaultSettings(); });
+            toSettingsButton.onClick.AddListener(delegate { ToSettings(); });
+            backButton.onClick.AddListener(delegate { BackButton(); });
+            quitButton.onClick.AddListener(delegate { QuitButton(); });
+            playButton.onClick.AddListener(delegate { PlayButtonPressed(); });
+
+            this.SettingsMenu.SetActive(false);
+            this.LoadingMenu.SetActive(false);
+
     }
 
+    public void PlayButtonPressed()
+    {
+        MapGenerator.enemy_cap = this.enemy_cap;
+        MapGenerator.num_marbles = this.num_marbles;
+        MapGenerator.width = this.width;
+        MapGenerator.height = this.height;
+        MapGenerator.passageWidth = this.passageWidth;
+        MapGenerator.seed = this.seed;
+        MapGenerator.useRandomSeed = this.useRandomSeed;
+        MapGenerator.randomFillPercent = this.randomFillPercent;
+        MapGenerator.wallThresholdSize = this.wallThresholdSize;
+        MapGenerator.roomThresholdSize = this.roomThresholdSize;
+        MapGenerator.forest_density = this.forest_density;
+        MapGenerator.disableMinimap = this.disableMinimap;
+        StartCoroutine("LoadYourAsyncScene");
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        this.LoadingMenu.SetActive(true);
+        this.MainMenu.SetActive(false);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+
+        while (!asyncLoad.isDone)
+        { 
+            yield return null;
+        }
+
+    }
+
+
+        public void ToSettings() {
+        this.MainMenu.SetActive(false);
+        this.SettingsMenu.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        this.MainMenu.SetActive(true);
+        this.SettingsMenu.SetActive(false);
+    }
+
+    public void QuitButton() {
+        #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+         Application.Quit();
+        #endif
+    }
 
     public void SetWidth(int w)
     {
@@ -144,7 +247,7 @@ public class GameManager : MonoBehaviour
 
     public void SetMapStatus(bool enabled)
     {
-        disableMinimap = !enabled;
+        disableMinimap = enabled;
 
     }
 
